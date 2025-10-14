216cd937fe733fcaf84a27897ccc89500c36db3e
alessandro-gaspari <alessandrogaspari2003@gmail.com>
2025-10-14 10:51:56 +0200

    try fix

--- app.py
+++ app.py
@@ -187,4 +187,5 @@ if __name__ == '__main__':
     init_database()
     port = int(os.environ.get('PORT', 10000))
     print(f"🚀 Server avviato sulla porta {port}")
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+
