60b3b665fc203ad2bf354e79557c90693f69084e
alessandro-gaspari <alessandrogaspari2003@gmail.com>
2025-10-22 11:50:05 +0200

    fix

--- static/dashboard.js
+++ static/dashboard.js
@@ -139,7 +139,7 @@ function initMap() {
     L.control.zoom({ position: 'topright' }).addTo(map);
     
     // VOYAGER DARK - Sfondo scuro con strade CHIARE e ben visibili
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '© OpenStreetMap',
         maxZoom: 19
     }).addTo(map);
